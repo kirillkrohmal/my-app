e873d90bbbcda3da5534856f5f0d11a621cce5de
kirillkrohmal <krohmal_kirill@mail.ru>
2022-12-18 11:57:56 +0300

    first commit

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -5,28 +5,31 @@ import Card from "../UI/Card";
 
 const Costs = (props)  => {
     return (
-        <Card className="costs">
-    
-            <CostItem
-            date={props.cost[0].date}
-            description={props.cost[0].description}
-            amount={props.cost[0].amount}
-            />
-            <CostItem
-                date={props.cost[1].date}
-                description={props.cost[1].description}
-                amount={props.cost[1].amount}
+        <div>
+            <CostFilter>
+            <Card className="costs">
+        
+                <CostItem
+                date={props.cost[0].date}
+                description={props.cost[0].description}
+                amount={props.cost[0].amount}
+                />
+                <CostItem
+                    date={props.cost[1].date}
+                    description={props.cost[1].description}
+                    amount={props.cost[1].amount}
+                    
+                />
+                <CostItem
                 
-            />
-            <CostItem
-            
-                date={props.cost[2].date}
-                description={props.cost[2].description}
-                amount={props.cost[2].amount}
+                    date={props.cost[2].date}
+                    description={props.cost[2].description}
+                    amount={props.cost[2].amount}
 
-            />
-        </Card>
-   
+                />
+            </Card>
+        </CostFilter>
+   </div>
     );
 }
 
